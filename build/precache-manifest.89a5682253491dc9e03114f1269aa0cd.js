a06be26132e56791531c1b15317287ea9a969943
onegamer11 <romanparwez9@gmail.com>
2021-07-22 15:31:30 +0530

    my todo list

---
+++ build/precache-manifest.89a5682253491dc9e03114f1269aa0cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e4381aab9944797e7648d8f2e4072a",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "a89035fc54a15eb56867",
+    "url": "/react-todolist/static/css/main.4c8a9d78.chunk.css"
+  },
+  {
+    "revision": "7af0030052c5ac2536b6",
+    "url": "/react-todolist/static/js/2.a9a881f2.chunk.js"
+  },
+  {
+    "revision": "a89035fc54a15eb56867",
+    "url": "/react-todolist/static/js/main.95fac144.chunk.js"
+  },
+  {
+    "revision": "dd2a0c730e02a59d4852",
+    "url": "/react-todolist/static/js/runtime~main.8b2e420c.js"
+  }
+]);
